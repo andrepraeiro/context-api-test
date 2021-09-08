b28ffcfb0c4c6ed5d06022ea3aaf76599e7aa422
Andre Praeiro <andrepraeiro@gmail.com>
2021-09-08 18:34:12 -0400

    testing

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,85 +1,76 @@
 import React from "react";
+import { UserContext } from "../../contexts/User";
+import { ActionType, LOGIN_USER_SUCCESS } from "../../contexts/User/actions";
 import "./style.scss";
 
 type UserLogin = {
-  name: string;
+  email: string;
   password: string;
 };
 
 const Login = () => {
   const [user, setUser] = React.useState<UserLogin>({
-    name: "",
+    email: "",
     password: "",
   });
 
-  const login = () => {
-    console.log(JSON.stringify(user));
+  const submitLogin = (dispatch: React.Dispatch<ActionType>) => {
+    const { email, password } = user;
+    dispatch({ type: LOGIN_USER_SUCCESS, payload: { email, password } });
   };
 
   return (
-    <div className="login-box">
-      <div className="session">
-        <div className="left"></div>
-        <form action="" className="log-in" autoComplete="off">
-          <h4>
-            We are <span>NUVA</span>
-          </h4>
-          <p>Welcome back! Log in to your account to view today's clients:</p>
-          <div className="floating-label">
-            <input
-              placeholder="Email"
-              type="email"
-              name="email"
-              id="email"
-              autoComplete="off"
-            />
-            <label htmlFor="email">Email:</label>
-            <div className="icon"></div>
+    <UserContext.Consumer>
+      {({ state, dispatch }) => (
+        <div className="login-box">
+          <div className="session">
+            <div className="left"></div>
+            <div className="form">
+              <h4>
+                We are <span>Acme Corporation</span>
+              </h4>
+              <p>Welcome back! Log in to your account:</p>
+              <div className="floating-label">
+                <input
+                  placeholder="Email"
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="off"
+                  value={user.email}
+                  onChange={(e) => {
+                    setUser({ ...user, email: e.target.value });
+                  }}
+                />
+                <label htmlFor="email">Email:</label>
+                <div className="icon"></div>
+              </div>
+              <div className="floating-label">
+                <input
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  id="password"
+                  autoComplete="off"
+                  value={user.password}
+                  onChange={(e) => {
+                    setUser({ ...user, password: e.target.value });
+                  }}
+                />
+                <label htmlFor="password">Password:</label>
+                <div className="icon"></div>
+              </div>
+              <button type="submit" onClick={() => submitLogin(dispatch)}>
+                Log in
+              </button>
+              <a href="www.google.com" className="discrete">
+                Basic version
+              </a>
+            </div>
           </div>
-          <div className="floating-label">
-            <input
-              placeholder="Password"
-              type="password"
-              name="password"
-              id="password"
-              autoComplete="off"
-            />
-            <label htmlFor="password">Password:</label>
-            <div className="icon"></div>
-          </div>
-          <button type="submit" onClick={() => login()}>
-            Log in
-          </button>
-          <a href="#" className="discrete">
-            Basic version
-          </a>
-        </form>
-      </div>
-
-      {/* <label>
-        <p>Username</p>
-        <input
-          type="text"
-          onChange={(event) => {
-            setUser({ ...user, name: event.target.value });
-          }}
-        ></input>
-      </label>
-      <label>
-        <p>Password</p>
-        <input
-          type="text"
-          onChange={(event) => {
-            setUser({ ...user, password: event.target.value });
-          }}
-        ></input>
-      </label>
-      <div>
-        <button type="button" onClick={() => login()}>
-          Login
-        </button>
-      </div> */}
-    </div>
+        </div>
+      )}
+    </UserContext.Consumer>
   );
 };
 
